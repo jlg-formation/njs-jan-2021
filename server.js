7506f37aba9421dacb4db01dc8d79d80cee7b19b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-01-05 15:16:14 +0100

    added middlewares

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-console.log("about to start the server");
-
 const express = require("express");
-console.log("express: ", express);
+const serveIndex = require("serve-index");
+
+console.log("about to start the server");
 
 const app = express();
 const port = 3000;
+const public = "./public";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use((req, res, next) => {
+  console.log("req.url: ", req.url);
+  next();
 });
+
+app.use(express.static(public));
+app.use(serveIndex(public));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
