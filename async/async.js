247fc1163a6b67d167e6f60ad13f04394e8711dd
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-01-06 09:53:16 +0100

    added path module

--- async/async.js
+++ async/async.js
@@ -1,21 +1,26 @@
 var fs = require("fs");
+var path = require("path");
 
-fs.appendFile("toto.txt", "async\n", (err) => {
+console.log("current dir: ", process.cwd());
+
+var filename = path.resolve(__dirname, "toto.txt");
+
+fs.appendFile(filename, "async\n", err => {
   if (err) {
     console.log("err: ", err);
     return;
   }
-  fs.appendFile("toto.txt", "async\n", (err) => {
+  fs.appendFile(filename, "async\n", err => {
     if (err) {
       console.log("err: ", err);
       return;
     }
-    fs.appendFile("toto.txt", "async\n", (err) => {
+    fs.appendFile(filename, "async\n", err => {
       if (err) {
         console.log("err: ", err);
         return;
       }
-      fs.appendFile("toto.txt", "async\n", (err) => {
+      fs.appendFile(filename, "async\n", err => {
         if (err) {
           console.log("err: ", err);
           return;
--- async/await.js
+++ async/await.js
@@ -1,24 +1,15 @@
 // Promise version >= ES2017
 const fs = require("fs");
 
-const appendFile = (filename, content) => {
-  return new Promise((resolve, reject) => {
-    fs.appendFile(filename, content, err => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve();
-    });
-  });
-};
+const appendFile = fs.promises.appendFile;
+const filename = "toto.txt";
 
 const main = async () => {
   try {
-    await appendFile("toto.txt", "await\n");
-    await appendFile("toto.txt", "await\n");
-    await appendFile("toto.txt", "await\n");
-    await appendFile("toto.txt", "await\n");
+    await appendFile(filename, "await\n");
+    await appendFile(filename, "await\n");
+    await appendFile(filename, "await\n");
+    await appendFile(filename, "await\n");
   } catch (err) {
     console.log("err: ", err);
   }
