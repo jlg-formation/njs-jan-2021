115d3a20539867176f669cb0296bdb295134dc8c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-01-06 16:01:25 +0100

    render articles from js object

--- frontEnd.ts
+++ frontEnd.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
 const app = Router();
 
+const articles = [
+  { name: "Tournevis cruciforme", price: 4.56, qty: 123 },
+  { name: "Pince", price: 1.45, qty: 123 },
+  { name: "Marteau", price: 2.78, qty: 123 },
+  { name: "Tournevis", price: 2, qty: 123 },
+  { name: "Tondeuse à gazon", price: 1234, qty: 123 },
+];
+
 app.get("/", (req, res) => {
-  res.render("pages/index");
+  res.render("pages/index", { articles: articles });
 });
 
 export default app;
