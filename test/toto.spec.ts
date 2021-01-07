8f7312b945a5a1a251c869694994595cc82cafdd
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-01-07 17:21:58 +0100

    feat: added mocha

---
+++ test/toto.spec.ts
@@ -0,0 +1,6 @@
+const assert = require("assert");
+describe("Article API", function () {
+  it("should return the articles", function () {
+    assert.deepStrictEqual([1, 2, 3].indexOf(4), -1);
+  });
+});
