f72a5f0ccf8964c53b034738590767e3d7f3a757
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-01-07 16:02:56 +0100

    crud mongo

--- exemple-db/connect.js
+++ exemple-db/connect.js
@@ -10,7 +10,26 @@ async function main() {
     });
 
     const db = client.db(dbName);
-    console.log("db: ", db);
+
+    // create
+    const article = { name: "Pince", price: 2.99, qty: 50 };
+    const result = await db.collection("articles").insertOne(article);
+    console.log("result: ", result.ops);
+
+    // retrieve
+    const r2 = await db.collection("articles").find({}).toArray();
+    console.log("r2: ", r2);
+
+    // update
+    const r3 = await db
+      .collection("articles")
+      .updateOne({}, { $inc: { qty: 1 } });
+    console.log("r3: ", r3);
+
+    // delete
+    var myquery = {};
+    const r4 = await db.collection("articles").deleteMany(myquery);
+    console.log("r4: ", r4);
 
     await client.close();
   } catch (err) {
