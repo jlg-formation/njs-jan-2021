cb57420cba9350cc42f7687a9d2ece67c1af8330
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-01-08 16:48:37 +0100

    added ecosystem file for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,10 @@ module.exports = {
     {
       script: "back/server.js",
       name: "gestion-stock",
+      env: {
+        NODE_ENV: "production",
+        NJS_SERVER_PORT: "5000",
+      },
     },
   ],
 };
