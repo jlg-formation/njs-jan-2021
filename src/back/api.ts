730ee81cd2626b574995f1523c742dd1213b8abd
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-01-08 16:31:16 +0100

    feat: added api crash

--- src/back/api.ts
+++ src/back/api.ts
@@ -4,5 +4,8 @@ const app = Router();
 app.get("/now", (req, res) => {
   res.json({ date: new Date() });
 });
+app.get("/crash", async (req, res) => {
+  process.exit(1);
+});
 
 export default app;
