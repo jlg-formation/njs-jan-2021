471cf71bc723fe190661b4230e2924024d8c91ea
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-01-08 09:22:21 +0100

    refactor: create back and front dir

--- test/toto.spec.ts
+++ test/toto.spec.ts
@@ -3,15 +3,21 @@ import fetch from "node-fetch";
 import { Article } from "../src/Article";
 describe("Article API", function () {
   it("should return the articles", async function () {
-    const response = await fetch("http://localhost:3000/actions/article-get");
-    console.log("response: ", response);
-    const json = await response.json();
-    console.log("json: ", json);
-    assert(json instanceof Array);
-    if (json.length === 0) {
-      return;
+    const server = new ArticleServer();
+    await server.start();
+    try {
+      const response = await fetch("http://localhost:3000/actions/article-get");
+      console.log("response: ", response);
+      const json = await response.json();
+      console.log("json: ", json);
+      assert(json instanceof Array);
+      if (json.length === 0) {
+        return;
+      }
+      const article: Article = json[0];
+      assert(article.name);
+    } finally {
+      await server.stop();
     }
-    const article: Article = json[0];
-    assert(article.name);
   });
 });
